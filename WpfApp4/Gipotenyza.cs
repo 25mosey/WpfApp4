ca1a2b70e12fcc3e4042f4ad1f93b2f67e6a6634
Juria1005 <shami1987@yandex.ru>
2022-06-11 10:08:26 +0200

    Добавьте файлы проекта.

---
+++ WpfApp4/Gipotenyza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4
+{
+    internal class Gipotenyza
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+
+        public Gipotenyza(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double Dlina()
+        {
+            return Math.Sqrt(A * A + B * B);
+        }
+    }
+}
